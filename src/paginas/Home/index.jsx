e35dcbeb4a60944b51ea983b602e6fa2764287fd
Matheus Morete Espinoso <128860497+MatheusMoorete@users.noreply.github.com>
2024-06-07 16:46:59 -0300

    Video correto + thumbnail

--- src/paginas/Home/index.jsx
+++ src/paginas/Home/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { FaPlay } from "react-icons/fa";
 import StyledArrowDown from "../../components/StyledArrow/StyledArrowDown";
 import TrasitionPages from "../../components/Transition/TrasitionPages";
 import MyFooter from "../../components/Footer/MyFooter";
 import ButttonScrollToTop from "../../components/ButttonScrollToTop/ButttonScrollToTop";
 import Boias from "../../components/Boias/Boias";
+import thumbnailImage from "../../assets/OceanoVivo_Logo.png"; 
 
 const Container = styled.div`
   position: relative;
@@ -32,10 +34,39 @@ const VideoSection = styled.div`
   justify-content: center;
   background: linear-gradient(to top, rgb(186, 210, 235), rgb(255, 255, 255));
   padding: 20px;
+  position: relative;
+`;
+
+const Thumbnail = styled.img`
+  max-width: 80%;
+  max-height: 80%;
+  cursor: pointer;
+  object-fit: contain;
+`;
+
+const PlayButtonContainer = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1100px;
+  height: 550px;
+`;
+
+const PlayButton = styled(FaPlay)`
+  color: white;
+  font-size: 4rem;
+  cursor: pointer;
+  opacity: 0.8;
+  transition: opacity 0.3s;
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const Home = () => {
   const [isArrowFixed, setIsArrowFixed] = useState(false);
+  const [showVideo, setShowVideo] = useState(false);
 
   const handleScrollToAbout = () => {
     const aboutSection = document.getElementById("videoSection");
@@ -47,14 +78,19 @@ const Home = () => {
     setIsArrowFixed(scrollPosition > 0);
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
+  const handleThumbnailClick = () => {
+    setShowVideo(true);
+  };
+
   const scrollThreshold = 670;
+  const videoId = "P2Nd7sdIaB0";
 
   return (
     <TrasitionPages>
@@ -66,16 +102,25 @@ const Home = () => {
         <StyledArrowDown onClick={handleScrollToAbout} fixed={isArrowFixed} />
       </BlocoBranco>
       <VideoSection id="videoSection">
-        <iframe
-          width="1100"
-          height="550"
-          src="https://www.youtube.com/embed/zyi6AvbFftk?si=v2leLKcgcGa5TYlO"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        {showVideo ? (
+          <iframe
+            width="1100"
+            height="550"
+            src={`https://www.youtube.com/embed/${videoId}?si=WuxdosaGqaxvrFj6`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        ) : (
+          <>
+            <Thumbnail src={thumbnailImage} alt="Video Thumbnail" onClick={handleThumbnailClick} />
+            <PlayButtonContainer onClick={handleThumbnailClick}>
+              <PlayButton />
+            </PlayButtonContainer>
+          </>
+        )}
       </VideoSection>
       <FooterContainer>
         <MyFooter />
